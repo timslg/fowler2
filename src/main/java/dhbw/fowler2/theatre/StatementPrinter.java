e9ec8e0158c9c8cfd6c1ba836aa32a750175e02c
samuel <ja>
2021-03-13 12:21:29 +0100

    User Story 5: Variablen umbenennen in Klasse StatementPrinter;

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -11,40 +11,40 @@ public class StatementPrinter {
         var volumeCredits = 0;
         var result = String.format("Statement for %s\n", invoice.customer);
 
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (var perf : invoice.performances) {
-            var play = perf.getPlay();
+        for (var aPerformance : invoice.performances) {
+            var aPlay = aPerformance.getPlay();
             var thisAmount = 0;
 
-            switch (play.type) {
+            switch (aPlay.type) {
                 case "tragedy":
                     thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30);
+                    if (aPerformance.audience > 30) {
+                        thisAmount += 1000 * (aPerformance.audience - 30);
                     }
                     break;
                 case "comedy":
                     thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
+                    if (aPerformance.audience > 20) {
+                        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
                     }
-                    thisAmount += 300 * perf.audience;
+                    thisAmount += 300 * aPerformance.audience;
                     break;
                 default:
                     throw new Error("unknown type: ${play.type}");
             }
 
             // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
+            volumeCredits += Math.max(aPerformance.audience - 30, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+            if ("comedy" == aPlay.type) volumeCredits += Math.floor(aPerformance.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            result += String.format("  %s: %s (%s seats)\n", aPlay.name, currencyFormat.format(thisAmount / 100), aPerformance.audience);
             totalAmount += thisAmount;
         }
-        result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
+        result += String.format("Amount owed is %s\n", currencyFormat.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
     }
