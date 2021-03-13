a9bcdbfcf6d1ae641e1dff921bccaf5dd3032cec
timschlegel <mail@timschlegel.com>
2021-03-13 14:10:46 +0100

    User-Story 10: JUnit Test für HTML-Ausgabe erstellen (TDD)

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -1,12 +1,14 @@
 package dhbw.fowler2.theatre;
 
-import java.text.NumberFormat;
-import java.util.Locale;
 
 public class StatementPrinter {
 
     public String print(Invoice invoice) {
         return new PlainTextStatement(invoice).value();
+    }
+
+    public String printHtml(Invoice invoice) {
+        return null;
     }
 
 }
--- src/test/java/dhbw/fowler2/theatre/StatementPrinterTest.java
+++ src/test/java/dhbw/fowler2/theatre/StatementPrinterTest.java
@@ -1,11 +1,6 @@
 package dhbw.fowler2.theatre;
 
-import dhbw.fowler2.theatre.Play;
-import dhbw.fowler2.theatre.Performance;
-import dhbw.fowler2.theatre.StatementPrinter;
-import dhbw.fowler2.theatre.Invoice;
 import java.util.List;
-import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -38,4 +33,26 @@ public class StatementPrinterTest {
                 "Amount owed is $1,730.00\n" +
                 "You earned 47 credits\n", result);
     }
+
+    @Test
+    public void printsHtmlStatements() {
+
+        Invoice invoice = new Invoice("BigCo", List.of(new Performance(plays.get(0), 55),
+                new Performance(plays.get(1), 35),
+                new Performance(plays.get(2), 40)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.printHtml(invoice);
+
+        Assert.assertEquals("Statement print mismatch", "<h1>Statement for BigCo</h1>\n" +
+                "<table>\n" +
+                "  <tr><th>Play</th><th>Seats</th><th>Price</th></tr>\n" +
+                "  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
+                "  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
+                "  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
+                "</table>\n" +
+                "<p>Amount owed is <em>$1,730.00</em></p>\n" +
+                "<p>You earned <em>47</em> credits</p>\n", result);
+    }
+
 }
